31140a9a954b79f3de48a7f4ee0ca256ea2fcf0a
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-13 20:19:41 +0530

    Add edit and update feature for user and config routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,26 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    if @user.save
+      flash.notice = "User '#{@user.username}' Updated!"
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
